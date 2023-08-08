d6feceb0ffe0ac18364df27576fd81e7c96ed758
Thúy Vi Elodie VO <thuyvielodie.vo@gmail.com>
2023-08-08 21:52:24 +0200

    [EDIT] C01 ex07 : main.c (tab with size >5 and comment translated)

--- C/C01/ex07/main.c
+++ C/C01/ex07/main.c
@@ -6,7 +6,7 @@
 /*   By: thuvo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:34:27 by thuvo             #+#    #+#             */
-/*   Updated: 2023/07/11 12:12:56 by thuvo            ###   ########.fr       */
+/*   Updated: 2023/07/27 02:35:58 by thuvo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@ void	ft_rev_int_tab(int *tab, int size);
 
 int     main(void)
 {
-        int     tab[5] = {0, 1, 2, 3, 4};
-        int     i;
-	int	size;
-
-        i = 0;
-	size = 5;
-	while (i < size)
-	{
-		printf("tab init : %d\n", tab[i]);
-		i++;
-	}
-	write (1, "\n", 1);
-        ft_rev_int_tab(tab, size);
-	i = 0;
-        while (i < size)
-        {
-                printf("Result : %d\n", tab[i]);
-                i++;
-        }
-        return (0);
+    int     tab[17] = {0, 2, 98, 33, 4, 598, 60, 7, 86, 9, 100, 11, 162, 3, 14, 45, 16};
+    int     i;
+    int     size;
+    
+    i = 0;
+    size = 17;
+    while (i < size)
+    {
+        printf("Tab init : %d\n", tab[i]);
+        i++;
+    }
+    write (1, "\n", 1);
+    ft_rev_int_tab(tab, size);
+    i = 0;
+    while (i < size)
+    {
+        printf("Result : %d\n", tab[i]);
+        i++;
+    }
+    return (0);
 }
 
 /*
-Fichier main.c pour eval C01_ex07
-ne pas utiliser printf sinon
+File main.c to check C01_ex07
+Do not send this for evaluation
 */
