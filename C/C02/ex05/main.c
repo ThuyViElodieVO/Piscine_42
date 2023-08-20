40f8657fb1b5693c0e52d875497ddacbf441ece6
Thúy Vi Elodie VO <thuyvielodie.vo@gmail.com>
2023-08-20 22:22:39 +0200

    [ADD] C02 ex05 : main.c to check if ex05 is ok

---
+++ C/C02/ex05/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuvo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 15:37:18 by thuvo             #+#    #+#             */
+/*   Updated: 2023/07/18 14:13:22 by thuvo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str);
+
+int	main()
+{
+	char	str_lowercase_letter[] = "bdewee";
+	printf("Resultat str_lowercase_letter : %d\n", ft_str_is_uppercase(str_lowercase_letter));
+
+	char	str_capital_letter[] = "QWERTYUIOOP";
+	printf("Resultat str_capital_letter : %d\n", ft_str_is_uppercase(str_capital_letter));
+
+	char	str_positive_digit[] = "12345";
+	printf("Resultat str_positive_digit : %d\n", ft_str_is_uppercase(str_positive_digit));
+
+	char	str_zero[] = "0";
+	ft_str_is_uppercase(str_zero);
+	printf("Resultat str_zero : %d n", ft_str_is_uppercase(str_zero));
+	
+	char	str_char[] = "%^&";
+	printf("Resultat str_char : %d\n", ft_str_is_uppercase(str_char));
+
+	char	str_blend[] = "5zxcvg5698&^%$";
+	printf("Resultat str_blend : %d\n", ft_str_is_uppercase(str_blend));
+
+	char	str_null[] = "";
+	printf("Resultat str_null : %d\n", ft_str_is_uppercase(str_null));
+
+	return (0);
+}
+
+// file main.c to check C02 ex05
+// Do not sent this for evaluation
